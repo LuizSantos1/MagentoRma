103499760afc53af10696a1acc43f59c3d256846
Matteo Fonsatti <matteo.fonsatti@wyscout.com>
2019-02-16 19:27:42 +0100

    created module mfonsatti_rma

---
+++ app/code/local/Mfonsatti/Rma/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mfonsatti
+ * Date: 16/02/19
+ * Time: 19:26
+ */ 
+class Mfonsatti_Rma_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/Mfonsatti/Rma/sql/mfonsatti_rma_setup/install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mfonsatti
+ * Date: 16/02/19
+ * Time: 19:26
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
