b420ceafd22cf20edc146061f47303f34f31f7d3
Matteo Fonsatti <matteo.fonsatti@wyscout.com>
2019-02-18 22:52:22 +0100

    added rma link in customer_account_navigation

---
+++ app/code/local/Mfonsatti/Rma/Block/Customer/Account/Navigation.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mfonsatti
+ * Date: 18/02/19
+ * Time: 22:14
+ */
+
+class Mfonsatti_Rma_Block_Customer_Account_Navigation extends Mage_Customer_Block_Account_Navigation
+{
+
+}
